82c7bd6349e3df7b77e4fba66b44ce65d533bc32
Robert <robgladue@hotmail.com>
2022-05-02 21:22:56 -0600

    add nft site and SC

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x543b0Febaa0b1887E3067bE9BA6Fa430F8598264";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
